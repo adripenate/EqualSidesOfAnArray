e6f99f728f383b123bb17443e7bfe50dcf20bd99
adriana <adrianapenate98@gmail.com>
2020-01-20 12:39:40 +0000

    Green test - find_index_when_array_has_four_elements_and_has_equals_sizes Two tests deleted due to bad approach

--- src/test/java/EqualSidesShould.java
+++ src/test/java/EqualSidesShould.java
@@ -11,18 +11,8 @@ public class EqualSidesShould {
     }
 
     @Test
-    public void not_find_index_when_array_has_two_different_elements() {
-        assertThat(EqualSides.findEvenIndex(new int[] {0, 1})).isEqualTo(-1);
-    }
-
-    @Test
     public void find_index_when_array_has_two_elements_and_the_last_is_zero() {
         assertThat(EqualSides.findEvenIndex(new int[] {0, 0})).isEqualTo(0);
-    }
-
-    @Test
-    public void not_find_index_when_array_has_three_elements_and_dont_has_equals_sizes() {
-        assertThat(EqualSides.findEvenIndex(new int[] {0, 0, -1})).isEqualTo(-1);
     }
 
     @Test
@@ -32,13 +22,13 @@ public class EqualSidesShould {
 
     @Test
     public void find_index_when_array_has_four_elements_and_has_equals_sizes() {
-        assertThat(EqualSides.findEvenIndex(new int[] {-1, 1, -1})).isEqualTo(2);
+        assertThat(EqualSides.findEvenIndex(new int[] {2, -2, -1})).isEqualTo(2);
     }
 
     private static class EqualSides {
         public static int findEvenIndex(int[] elements) {
             if (hasOnlyOne(elements)) return 0;
-            for (int position = 0; position<elements.length-1; position++){
+            for (int position = 0; position<elements.length; position++){
                 int sumOfRightElements = getSumOfElementsFrom(elements, position+1, elements.length);
                 int sumOfLeftElements = getSumOfElementsFrom(elements, 0, position);
                 if (sumOfRightElements == sumOfLeftElements) return position;
