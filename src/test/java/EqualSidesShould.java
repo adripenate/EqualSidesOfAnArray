029b5b36cf036e155da493d6198c8ce112d05263
adriana <adrianapenate98@gmail.com>
2020-01-20 11:49:38 +0000

    Red test - find_index_0_when_array_has_one_element

---
+++ src/test/java/EqualSidesShould.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class EqualSidesShould {
+    @Test
+    public void find_index_0_when_array_has_one_element() {
+        assertThat(EqualSides.findEvenIndex(new int[] {0})).isEqualTo(0);
+    }
+
+    private static class EqualSides {
+        public static int findEvenIndex(int[] elements) {
+            return -1;
+        }
+    }
+}
