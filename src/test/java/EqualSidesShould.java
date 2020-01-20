ce2b35742eea7a193657bcd9f6220c546d7bc95d
adriana <adrianapenate98@gmail.com>
2020-01-20 11:53:00 +0000

    Red test - not_find_index_when_array_has_two_different_elements

--- src/test/java/EqualSidesShould.java
+++ src/test/java/EqualSidesShould.java
@@ -8,6 +8,11 @@ public class EqualSidesShould {
         assertThat(EqualSides.findEvenIndex(new int[] {0})).isEqualTo(0);
     }
 
+    @Test
+    public void not_find_index_when_array_has_two_different_elements() {
+        assertThat(EqualSides.findEvenIndex(new int[] {0, 1})).isEqualTo(-1);
+    }
+
     private static class EqualSides {
         public static int findEvenIndex(int[] elements) {
             return 0;
